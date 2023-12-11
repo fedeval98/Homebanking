a670c0b0537aeb2e5fa2bc1e4df015fa1c60686c
fedeval98 <fede.val9806@gmail.com>
2023-12-11 13:03:26 -0300

    terminado front del sprint3

--- src/main/resources/static/web/assets/js/account.js
+++ src/main/resources/static/web/assets/js/account.js
@@ -1,50 +1,111 @@
-const API = "/api/clients/"
-const ID = 1
+const CLIENTS = "/api/clients/"
+const ACCOUNTS = "/api/accounts/"
 const {createApp} = Vue
 
 const options = {
   data(){
     return {
+      idclient:1,
+      idAccount:null,
       client: [],
       accounts:[],
+      account:[],
+      transaction:[],
       isWideScreen:false,
+      modalVisibleAlert: false,
     } // finaliza return
   }, // finaliza data
-
   created(){
+    const search = location.search
+    const params = new URLSearchParams(search)
+    this.idAccount = params.get('id')
     this.loadData()
+    this.loadTransaction()
   }, //finaliza created
 
   methods:{
     loadData(){
-      axios.get(API + ID)
+      axios.get(CLIENTS + this.idclient)
       .then (data => {
         this.client = data.data
         this.accounts = data.data.accounts
-        console.log(this.client)
-        console.log(this.accounts)
+        console.log(this.idAccount)
+        console.log("clientes",this.client)
+        console.log("cuentas",this.accounts)
+        this.account = this.accounts.find(account => account.id == this.idAccount)
+        console.log("cuenta: ",this.account)
       }) // finaliza then data
-      .catch (error => console.log ("Error: ",error))
+      .catch (error => console.log ("Error: ",error))      
+    },
+    loadTransaction(){
+      axios.get(ACCOUNTS + this.idAccount + "/transactions")
+        .then (data => {
+          this.transaction = []
+          this.transaction = data.data
+          this.transaction.sort((a,b) =>{
+            return b.id - a.id
+          })
+          console.log("transaccion",this.transaction)
+        })
+        .catch (error => console.log("Error: ",error))
     },
     formatBudget(balance){
       if(balance !== undefined && balance !== null){
-        return balance.toLocaleString("en-US",{
+        const sign = balance < 0 ? "-":""
+        const formattedBalance = Math.abs(balance).toLocaleString("es-MX",{
           style: "currency",
           currency: "ARS",
+          currencyDisplay:"narrowSymbol",
           minimumFractingDigits: 0,
         })
+        return `ARS ${sign}${formattedBalance}`
       }
     },
     checkScreenSize(){
     this.isWideScreen = window.innerWidth >=1024
-    }
+    },
+    abrirAlert() {
+      this.modalVisibleAlert = true
+      if (this.modalVisibleAlert) {
+        document.body.classList.add('overflow-y-hidden')
+      }
+    }, // finaliza showModal
+    cerrarAlert() {
+      this.modalVisibleAlert = false
+      if (this.modalVisibleAlert == false) {
+        document.body.classList.remove('overflow-y-hidden')
+      }
+    },// finaliza cerrarModal
+    formatDate(array){
+      // se formatea la fecha de las peliculas usando un formato
+      // en este caso utiliza el formato de fecha/hora local
+      // dado que se usa .toLocaleDateString y se establece
+      // la configuracion regional "es-ES" y se le da el formato
+      // options (dia:numerico, mes:texto, año:numerico)
+      // ejemplo (11 julio de 1998)
+      const options = {day:'numeric', month:'long', year:'numeric'}
+      const release = new Date(array.creationDate)
+      return release.toLocaleDateString("es-ES",options)
+    },
+    formatDateTime(array){
+      // se formatea la fecha de las peliculas usando un formato
+      // en este caso utiliza el formato de fecha/hora local
+      // dado que se usa .toLocaleDateString y se establece
+      // la configuracion regional "es-ES" y se le da el formato
+      // options (dia:numerico, mes:texto, año:numerico)
+      // ejemplo (11 julio de 1998)
+      const options = {day:'numeric', month:'long', year:'numeric', hour:'numeric', minute:'numeric', second:'numeric',}
+      const release = new Date(array.dateTime)
+      return release.toLocaleString("es-ES",options)
+    },
   }, //fin methods
+
   mounted(){
   this.checkScreenSize()
   window.addEventListener('resize', this.checkScreenSize)
   },
   beforeDestroy(){
-  window.removeEventListener('risize', this.checkScreenSize)
+  window.removeEventListener('resize', this.checkScreenSize)
   }
 
 } //finaliza createApp
--- src/main/resources/static/web/assets/js/accounts.js
+++ src/main/resources/static/web/assets/js/accounts.js
@@ -1,50 +1,77 @@
-const API = "/api/clients/"
-const ID = 1
+const CLIENTS = "/api/clients/"
+const ACCOUNTS = "/api/accounts/"
 const {createApp} = Vue
 
 const options = {
   data(){
     return {
+      idclient:1,
       client: [],
       accounts:[],
       isWideScreen:false,
+      modalVisibleAlert: false,
     } // finaliza return
   }, // finaliza data
-
   created(){
     this.loadData()
   }, //finaliza created
 
   methods:{
     loadData(){
-      axios.get(API + ID)
+      axios.get(CLIENTS + this.idclient)
       .then (data => {
         this.client = data.data
         this.accounts = data.data.accounts
-        console.log(this.client)
-        console.log(this.accounts)
+        this.accounts.sort((a,b)=> a.id - b.id)
       }) // finaliza then data
-      .catch (error => console.log ("Error: ",error))
+      .catch (error => console.log ("Error: ",error))      
     },
     formatBudget(balance){
       if(balance !== undefined && balance !== null){
-        return balance.toLocaleString("en-US",{
+        const sign = balance < 0 ? "-":""
+        const formattedBalance = Math.abs(balance).toLocaleString("es-MX",{
           style: "currency",
           currency: "ARS",
+          currencyDisplay:"narrowSymbol",
           minimumFractingDigits: 0,
         })
+        return `ARS ${sign}${formattedBalance}`
       }
     },
     checkScreenSize(){
     this.isWideScreen = window.innerWidth >=1024
-    }
+    },
+    abrirAlert() {
+      this.modalVisibleAlert = true
+      if (this.modalVisibleAlert) {
+        document.body.classList.add('overflow-y-hidden')
+      }
+    }, // finaliza showModal
+    cerrarAlert() {
+      this.modalVisibleAlert = false
+      if (this.modalVisibleAlert == false) {
+        document.body.classList.remove('overflow-y-hidden')
+      }
+    },// finaliza cerrarModal
+    formatDate(array){
+      // se formatea la fecha de las peliculas usando un formato
+      // en este caso utiliza el formato de fecha/hora local
+      // dado que se usa .toLocaleDateString y se establece
+      // la configuracion regional "es-ES" y se le da el formato
+      // options (dia:numerico, mes:texto, año:numerico)
+      // ejemplo (11 julio de 1998)
+      const options = {day:'numeric', month:'long', year:'numeric'}
+      const release = new Date(array.creationDate)
+      return release.toLocaleDateString("es-ES",options)
+    },
   }, //fin methods
+
   mounted(){
   this.checkScreenSize()
   window.addEventListener('resize', this.checkScreenSize)
   },
   beforeDestroy(){
-  window.removeEventListener('risize', this.checkScreenSize)
+  window.removeEventListener('resize', this.checkScreenSize)
   }
 
 } //finaliza createApp
